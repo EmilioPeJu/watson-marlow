485fecb1eff3d424482b690a919e4f8b446ac892
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2012-06-22 11:02:09 +0000

    watson-marlow: initial structure of module with start/stop, speed and dir

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,12 @@
+from iocbuilder import AutoSubstitution
+from iocbuilder.modules.streamDevice import AutoProtocol
 
+class Pump323Du(AutoSubstitution, AutoProtocol):
+    '''Controls a Watson Marlow 323Du Pump'''
+
+    ## Parse this template file for macros
+    TemplateFile = '323Du.template'
+
+    ## This is the streamDevice protocol file to use
+    ProtocolFiles = ['323Du.proto']
 
